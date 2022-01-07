6f7a986031ce7c63972dac4dc5e476a937cf8b6c
dmu2014 <dishank2007@gmail.com>
2022-01-07 15:43:37 -0500

    module 3 code

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import OptionsPanel from '../OptionsPanel'
 import Board from '../Board'
-import { createTiles } from '../../misc/utils'
+import { createTiles, indexOfSelected } from '../../misc/utils'
 
 import './App.css';
 
@@ -12,11 +12,47 @@ class App extends Component{
     //this.state = {numTiles: 36, playing: false, previousTileIndex: null, tiles:[], toBeCleared:null};
     this.state={};
     this.state = {numTiles: 36, playing: false, previousTileIndex: null, tiles:[], toBeCleared:null};
-    
+  }
+
+  handleTileClicked(id, color){
+    this.setState((state) => {
+      const tiles = state.tiles;
+      let toBeCleared = state.toBeCleared;
+      const selectedTileIndex = indexOfSelected(tiles, id, color);
+      let previousTileIndex = state.previousTileIndex;      
+      if(toBeCleared !== null){
+        tiles[toBeCleared[0]].selected = false;
+        tiles[toBeCleared[1]].selected = false;
+        toBeCleared = null;
+      }
+      tiles[selectedTileIndex].selected = true;
+
+      if(previousTileIndex !== null){
+        const previousTile = tiles[previousTileIndex];
+        const selectedTile = tiles[selectedTileIndex];
+        if(previousTile.id !== selectedTile.id && previousTile.color === color){
+          selectedTile.matched = true;
+          previousTile.matched = true;
+          previousTileIndex = null;
+        }
+        else{
+          toBeCleared = [previousTileIndex, selectedTileIndex];
+          previousTileIndex = null;
+        }
+      }
+      else{
+        previousTileIndex = selectedTileIndex;
+      }
+      return {        
+        toBeCleared: toBeCleared,
+        tiles: tiles,
+        previousTileIndex: previousTileIndex
+      }
+    });
   }
 
   startGame(numTiles){
-    this.setState((state) => {state.playing=true, state.previousTileIndex=null, state.toBeCleared=null, state.tiles = createTiles(state.numTiles)})      
+    this.setState((state) => {state.playing=true, state.previousTileIndex=null, state.toBeCleared=null, state.tiles = createTiles(state.numTiles, this.handleTileClicked)})      
       //{playing:true, previousTileIndex:null, toBeCleared:null, tiles:createTiles(numTiles)});
   }
 
@@ -30,7 +66,6 @@ class App extends Component{
         <Board numTiles={this.state.numTiles} tiles={this.state.tiles}/>      
     </div>
   );
-
   }
 }
 
